d1dcb32e633d8aabd5456e5cc45eebd73e877db2
Jose Sustaita <josesustaita70@gmail.com>
2020-04-28 22:15:56 -0400

    set up middleware

--- app/src/index.js
+++ app/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-
 import { reducer } from "./reducer/appReducer";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
